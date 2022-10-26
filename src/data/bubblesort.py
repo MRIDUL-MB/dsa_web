f7eb3c8bee23f2c2db4656f79c6c9382dcbf7390
Subinoy Biswas <heysubinoy@gmail.com>
2022-10-26 19:07:48 +0530

    added Leetcode problem

---
+++ src/data/bubblesort.py
@@ -0,0 +1,10 @@
+def bubblesort(array):
+    for i in range(len(array)):
+        for j in range(0,len(array)- i -1):
+            if array[j] > array[j+1]:
+                temp = array[j]
+                array[j] = array[j+1]
+                array[j+1] = temp
+            
+input =  [45,23,55,2,34]
+bubblesort(input)
